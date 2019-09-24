804d769b171d9adf36c0429671f3a6b7dc0b3088
Joel Drake <joel@infoping.se>
2019-09-24 17:37:48 +0200

    with analyze

--- next.config.js
+++ next.config.js
@@ -1,15 +1,20 @@
 const path = require('path');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 const withSass = require('@zeit/next-sass');
 
-module.exports = withSass({
-  webpack(config) {
-    config.resolve.alias['@components'] = path.join(__dirname, 'components');
-    config.resolve.alias['@styles'] = path.join(__dirname, 'styles');
-    config.resolve.alias['styles'] = path.join(__dirname, 'styles');
-    config.resolve.alias['@actions'] = path.join(__dirname, 'actions');
-    config.resolve.alias['@utils'] = path.join(__dirname, 'utils');
-    config.resolve.alias['@'] = path.join(__dirname, '/');
+module.exports = withBundleAnalyzer(
+  withSass({
+    webpack(config) {
+      config.resolve.alias['@components'] = path.join(__dirname, 'components');
+      config.resolve.alias['@styles'] = path.join(__dirname, 'styles');
+      config.resolve.alias['styles'] = path.join(__dirname, 'styles');
+      config.resolve.alias['@actions'] = path.join(__dirname, 'actions');
+      config.resolve.alias['@utils'] = path.join(__dirname, 'utils');
+      config.resolve.alias['@'] = path.join(__dirname, '/');
 
-    return config;
-  },
-});
+      return config;
+    },
+  }),
+);
